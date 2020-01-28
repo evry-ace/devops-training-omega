88b92247ee3707a2cb3692247a7fa677a2f04c99
David Halfdan Haugli <david.haugli@evry.com>
2020-01-28 23:03:45 +0100

    env

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
 
-  subscription_id = "ARM_SUBSCRIPTION_ID"
-  client_id       = "ARM_CLIENT_ID"
-  client_secret   = "ARM_CLIENT_SECRET"
-  tenant_id       = "ARM_TENANT_ID"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
 }
 
