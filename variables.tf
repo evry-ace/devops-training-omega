5832f01159caa37690c4730c6bdd1fc585cbb108
David Halfdan Haugli <david.haugli@evry.com>
2020-01-29 10:14:26 +0100

    test2

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
-
+/*
 variable "subscription_id" {
-  type = string
+  type    = string
   default = "ARM_SUBSCRIPTION_ID"
 }
 
 variable "client_secret" {
-  type = string
+  type    = string
   default = "ARM_CLIENT_SECRET"
 }
 
 variable "client_id" {
-  type = string
+  type    = string
   default = "ARM_CLIENT_ID"
 }
 
 variable "tenant_id" {
-  type = string
+  type    = string
   default = "ARM_TENANT_ID"
 }
 
-  
+*/
