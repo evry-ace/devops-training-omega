592ab6dc1f12c0b393f9eba300c2498ae3d7ed61
Artjoms Laivins <e211223@Artjomss-MacBook-Pro.local>
2020-04-13 23:19:15 +0200

    aks cluster

---
+++ omega-aks/main.tf
@@ -0,0 +1,74 @@
+provider "azurerm" {
+  version         = "=2.0.0"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {}
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "omega-devops"
+
+    workspaces {
+      name = "devops-training-omega-aks"
+    }
+  }
+}
+
+locals {
+
+  tags = {
+    team = "devops-training-omega"
+  }
+
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "devops-training-omega"
+}
+
+data "azurerm_virtual_network" "omegavnet" {
+    name = "omegaVnet"
+}
+
+
+module "aks" {
+  source = "github.com/evry-ace/tf-azure-aks?ref=0.8.0-az-cni"
+
+  resource_group_name     = azurerm_resource_group.rg.name
+  resource_group_location = azurerm_resource_group.rg.location
+
+  ssh_public_key          = "${var.ssh_public_key}"
+  cluster_name            = "aks-${local.name_affix}"
+  dns_prefix              = local.name_affix
+
+  client_id     = var.sp_client_id
+  client_secret = var.sp_client_secret
+
+  rbac_server_app_id     = var.sp_client_id
+  rbac_server_app_secret = var.sp_client_secret
+  rbac_client_app_id     = var.sp_client_id
+
+  k8s_version = "1.15.5"
+  aks_docker_bridge_cidr = "172.17.0.1/16"
+
+  aks_service_cidr   = local.svc_cidr
+  aks_dns_service_ip = cidrhost(local.svc_cidr, 4)
+  
+  aks_network_plugin = "azure"
+  aks_network_policy = "calico"
+  aks_vnet_subnet_id = data.azurerm_virtual_network.omegavnet.id
+ 
+  create_vnet        = false
+  oms_agent_enable = false
+  default_pool = {
+    node_count = 2
+    vm_size    = "Standard_B1ms"
+  }
+
+  tags = local.tags
+}
---
+++ omega-aks/variables.tf
@@ -0,0 +1,29 @@
+variable "prefix" {
+  default = "aks"
+}
+
+variable "kubernetes_client_id" {
+  description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
+  type = string
+}
+
+variable "kubernetes_client_secret" {
+  description = "The Client Secret for the Service Principal to use for this Managed Kubernetes Cluster"
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
